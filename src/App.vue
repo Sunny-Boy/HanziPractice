38836028b5616472b41f9d1fcf612e1bb5f4905d
前端完成 <321417672@qq.com>
2025-02-18 14:10:21 +0800

    修复手机无法显示字体样式问题

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,26 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
+
+const fontFamilyOptions = [
+  { label: '宋体', value: '-apple-system, "SimSun", "STSong", serif' },
+  { label: '楷体', value: '-apple-system, "KaiTi", "STKaiti", serif' },
+  { label: '黑体', value: '-apple-system, "SimHei", "STHeiti", sans-serif' },
+  { label: '仿宋', value: '-apple-system, "FangSong", "STFangsong", serif' },
+  { label: '硬笔楷体', value: '"Hard-Pen-Kaishu", "KaiTi", "STKaiti", serif' },
+]
 </script>
 
 <template>
   <HelloWorld />
 </template>
 
-<style scoped>
+<style>
+/* 确保字体在移动端也能正常显示 */
+@media screen and (max-width: 768px) {
+  * {
+    -webkit-text-size-adjust: 100%;
+    -moz-text-size-adjust: 100%;
+    -ms-text-size-adjust: 100%;
+  }
+}
 </style>
